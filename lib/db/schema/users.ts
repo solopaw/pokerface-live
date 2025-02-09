965ef1bc8b0f4e830f1f054b8a26196efc25c8e7
solopaw <adamjoelfraser@icloud.com>
2025-02-09 12:28:02 +1000

    modified:   .env.example 	modified:   lib/db/schema/users.ts

--- lib/db/schema/users.ts
+++ lib/db/schema/users.ts
@@ -6,7 +6,7 @@ export const users = sqliteTable('users', {
     password: text('password').notNull(),
     name: text('name').notNull(),
     username: text('username').notNull().unique(),
-    created_at: integer('created_at',{mode:'timestamp'}).default(sql`current_timestamp`),
-    updated_at: integer('updated_at',{mode:'timestamp'}).default(sql`current_timestamp`),
+    created_at: integer('created_at',{mode:'timestamp'}).default(sql`(current_timestamp)`),
+    updated_at: integer('updated_at',{mode:'timestamp'}).default(sql`(current_timestamp)`),
     }
 )
